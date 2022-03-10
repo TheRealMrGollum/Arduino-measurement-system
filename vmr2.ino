681f22846341166567ddc83d804288fbcdf3f6cb
TheRealMrGollum <101318597+TheRealMrGollum@users.noreply.github.com>
2022-03-10 09:34:43 +0100

    Update vmr2.ino

--- vmr2.ino
+++ vmr2.ino
@@ -3,87 +3,87 @@
 #define INTERVAL_1 10
 #define INTERVAL_2 1000
 #define INTERVAL_3 60000
-unsigned long time_1 = 0;
-unsigned long time_2 = 0;
-unsigned long time_3 = 0;
+unsigned long time_1=0, time_2=0, time_3=0;
 File myFile;
 int analogInput = 0;
-String Meranie = "Meranie",Medzera="  ";
-float Vvyst = 0.000, Ivyst = 0.00, Wvyst=0.00 , WH=0.00, mWspolu;
-float Vout, Vin, nap;
-float R1 = 3, R2 = 22,Vref=1.1;
-bool prveMeranie = true;
-void setup(){
-   pinMode(analogInput, INPUT);
-   Serial.begin(57600); 
-   if (!SD.begin(4)) {
+String Measurement = "Measurement", Space="  ";
+float Vout = 0.000, Iout = 0.00, Wvyst=0.00 , WH=0.00, Vin, nap, R1 = 3, R2 = 22,Vref=1.1;
+bool FirstMeasurement = true;
+
+void setup()
+{
+  pinMode(analogInput, INPUT);
+  Serial.begin(57600); 
+  if (!SD.begin(4)) //If SD card is damaged or not inserted, do not continue
+  {
     Serial.println("initialization failed!");
     while (1);
   }
-  myFile = SD.open(Meranie, FILE_WRITE);
-  Serial.println("Begin");
+  myFile = SD.open(Measurement, FILE_WRITE);
+  Serial.println("Card inserted and it is working!");
   analogReference(INTERNAL);
-  
 }
-void print_time(unsigned long time_millis);
+
+void print_time(unsigned long time_millis); //only showed in serial monitoring
+
 void loop()
 {     
       if(millis() >= time_1 + INTERVAL_1)
       {
         time_1 +=INTERVAL_1;
-        meranie();
+        measurement();
       } 
       if(millis() >= time_2 + INTERVAL_2)
       {
         time_2 +=INTERVAL_2;
         print_time(time_2);
         //Serial.println("prebieha vypocet");
-        pocitanie();
+        calculate();
       }
       if(millis() >= time_3 + INTERVAL_3)
       {
         time_3 +=INTERVAL_3;
         print_time(time_3);
-        Serial.println("Prebieha zapis na SD kartu");
-        sdkarta();
+        Serial.println("Saving data on SD card");
+        sdcard();
       }
           
 }
 
-void meranie()
+void measurement()
 {   
-    if ((analogRead(analogInput) <40)&(analogRead(analogInput) >35))Vout = ((analogRead(analogInput)+5) * Vref) / 1024.00;
-    else if ((analogRead(analogInput) <35)&(analogRead(analogInput) >20))Vout = ((analogRead(analogInput)+1) * Vref) / 1024.00;
-    else if ((analogRead(analogInput) <20)&(analogRead(analogInput) >3))Vout = ((analogRead(analogInput)+2) * Vref) / 1024.00;
-    else if (analogRead(analogInput) > 0)Vout = ((analogRead(analogInput)) * Vref) / 1024.00;
-    else Vout = ((analogRead(analogInput)) * Vref) / 1024.00;
-    Vin = (Vout) / (R2/(R1+R2));
+    if ((analogRead(analogInput) <40)&(analogRead(analogInput) >35))Vin = ((analogRead(analogInput)+5) * Vref) / 1024.00;
+    else if ((analogRead(analogInput) <35)&(analogRead(analogInput) >20))Vin = ((analogRead(analogInput)+1) * Vref) / 1024.00;
+    else if ((analogRead(analogInput) <20)&(analogRead(analogInput) >3))Vin = ((analogRead(analogInput)+2) * Vref) / 1024.00;
+    else if (analogRead(analogInput) > 0)Vin = ((analogRead(analogInput)) * Vref) / 1024.00;
+    else Vin = ((analogRead(analogInput)) * Vref) / 1024.00;
+    Vin /= (R2/(R1+R2));
     nap+= Vin;
 }
 
-void pocitanie()
+void calculate()
 {
-  nap = nap/100;
-  Serial.print("stredná hodnota ");
-        Serial.println(nap*1000);
-  Vvyst=nap*1000; Ivyst =(nap)/(R1+R2); Wvyst = (nap) * (Ivyst); Ivyst=Ivyst*1000;
-  // Veľkosť vytvorenej energie
-  mWspolu += (Wvyst*1000);
-  WH = mWspolu /3600;
-  WH = WH*1000000;
-  if (prveMeranie == true) 
+  nap = nap/100; //Median of voltage
+  Serial.print("Median of voltage ");
+  Serial.println(nap*1000); // display in mV in serial
+  Vout=nap*1000; Iout =(nap)/(R1+R2); Wvyst = (nap) * (Iout); Iout*=1000; //V -> mV, A , W, A -> mA, 
+  
+  WH += (Wvyst*1000); // W -> mW
+  WH /= 3600;      // mW + time = mWH
+  WH *= 1000000;    // mW -> nWH (E-9)
+  if (FirstMeasurement == true) 
     {
-      myFile.println("##########Meranie##########");
-      prveMeranie=false;
+      myFile.println("##########Measurement##########");
+      FirstMeasurement=false;
     }
-  myFile.println(Vvyst+Medzera+Ivyst+Medzera+WH);
+  myFile.println(Vout+Space+Iout+Space+WH); //mV, mA, nWH
 }
 
-void sdkarta()
+void sdcard()
 {
-  myFile.close();
-  Serial.println("Súbor uložený: " + Meranie);
-  myFile = SD.open(Meranie, FILE_WRITE);
+  myFile.close(); //Save data
+  Serial.println("Súbor uložený: " + Measurement); 
+  myFile = SD.open(Measurement, FILE_WRITE); //Open file and continue where ended
 }
 
 void print_time(unsigned long time_millis)
