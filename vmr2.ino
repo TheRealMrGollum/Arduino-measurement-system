b4133b179ae1de0ea95e27f8fe378f1b59ed078e
TheRealMrGollum <101318597+TheRealMrGollum@users.noreply.github.com>
2022-03-10 08:55:36 +0100

    Update vmr2.ino

--- vmr2.ino
+++ vmr2.ino
@@ -1,19 +1,22 @@
 #include <SPI.h>
 #include <SD.h>
-
+#define INTERVAL_1 10
+#define INTERVAL_2 1000
+#define INTERVAL_3 60000
+unsigned long time_1 = 0;
+unsigned long time_2 = 0;
+unsigned long time_3 = 0;
 File myFile;
 int analogInput = 0;
-String Meranie = "Meranie";
-int sekundy=-1, minuty=0, hodiny=0;
+String Meranie = "Meranie",Medzera="  ";
 float Vvyst = 0.000, Ivyst = 0.00, Wvyst=0.00 , WH=0.00, mWspolu;
-float Vout, Vin;
+float Vout, Vin, nap;
 float R1 = 3, R2 = 22,Vref=1.1;
-int val = 0;
 bool prveMeranie = true;
 void setup(){
    pinMode(analogInput, INPUT);
-   Serial.begin(9600); 
-   if (!SD.begin(10)) {
+   Serial.begin(57600); 
+   if (!SD.begin(4)) {
     Serial.println("initialization failed!");
     while (1);
   }
@@ -22,67 +25,70 @@ void setup(){
   analogReference(INTERNAL);
   
 }
-void loop(){
-    //Počítanie
-    if (analogRead(analogInput) > 0)
-    Vout = ((analogRead(analogInput)+1) * Vref) / 1024.00;
-    else 
-    Vout = ((analogRead(analogInput)) * Vref) / 1024.00;
-    Vin = (Vout) / (R2/(R1+R2)); 
-    Vvyst = ((Vin)*1000);
-    
-    // prúd
-    Ivyst = (Vin)/(R1+R2);
-    Ivyst = Ivyst * 1000;
-    // energia
-    Wvyst = (Vvyst/1000) * (Ivyst/1000);
-    // cas
-    sekundy += 1;
-    
-    
-    if (sekundy == 60) 
+void print_time(unsigned long time_millis);
+void loop()
+{     
+      if(millis() >= time_1 + INTERVAL_1)
       {
-        minuty += 1;
-        sekundy = 0;
-      }
-    if (minuty == 60)
+        time_1 +=INTERVAL_1;
+        meranie();
+      } 
+      if(millis() >= time_2 + INTERVAL_2)
       {
-        hodiny +=1;
-        minuty=0;
+        time_2 +=INTERVAL_2;
+        print_time(time_2);
+        //Serial.println("prebieha vypocet");
+        pocitanie();
       }
-    // Veľkosť vytvorenej energie
-    mWspolu += (Wvyst*1000);
-    WH = mWspolu /3600;
-    WH = WH*1000000;
-    Serial.print(Vvyst);
-    Serial.print("  ");
-    Serial.print(Ivyst);
-    Serial.print("  ");
-    Serial.println(WH);
-     
-  
-//////////////////////////////////
-// SD karta
-//////////////////////////////////
-    if (prveMeranie == true) 
+      if(millis() >= time_3 + INTERVAL_3)
+      {
+        time_3 +=INTERVAL_3;
+        print_time(time_3);
+        Serial.println("Prebieha zapis na SD kartu");
+        sdkarta();
+      }
+          
+}
+
+void meranie()
+{   
+    if ((analogRead(analogInput) <40)&(analogRead(analogInput) >35))Vout = ((analogRead(analogInput)+5) * Vref) / 1024.00;
+    else if ((analogRead(analogInput) <35)&(analogRead(analogInput) >20))Vout = ((analogRead(analogInput)+1) * Vref) / 1024.00;
+    else if ((analogRead(analogInput) <20)&(analogRead(analogInput) >3))Vout = ((analogRead(analogInput)+2) * Vref) / 1024.00;
+    else if (analogRead(analogInput) > 0)Vout = ((analogRead(analogInput)) * Vref) / 1024.00;
+    else Vout = ((analogRead(analogInput)) * Vref) / 1024.00;
+    Vin = (Vout) / (R2/(R1+R2));
+    nap+= Vin;
+}
+
+void pocitanie()
+{
+  nap = nap/100;
+  Serial.print("stredná hodnota ");
+        Serial.println(nap*1000);
+  Vvyst=nap*1000; Ivyst =(nap)/(R1+R2); Wvyst = (nap) * (Ivyst); Ivyst=Ivyst*1000;
+  // Veľkosť vytvorenej energie
+  mWspolu += (Wvyst*1000);
+  WH = mWspolu /3600;
+  WH = WH*1000000;
+  if (prveMeranie == true) 
     {
       myFile.println("##########Meranie##########");
       prveMeranie=false;
     }
-    myFile.print(Vvyst);
-    myFile.print("  ");
-    myFile.print(Ivyst);
-    myFile.print("  ");
-    myFile.println(WH);
-    if ((minuty == 9)|(minuty == 19)|(minuty == 29)|(minuty == 39)|(minuty == 49)|(minuty == 59))
-    { 
-      if (sekundy == 30)
-      {
-      myFile.close();
-      Serial.println("Súbor uložený: " + Meranie);
-      myFile = SD.open(Meranie, FILE_WRITE);
-      }
-    }
-    delay(1000);
+  myFile.println(Vvyst+Medzera+Ivyst+Medzera+WH);
+}
 
+void sdkarta()
+{
+  myFile.close();
+  Serial.println("Súbor uložený: " + Meranie);
+  myFile = SD.open(Meranie, FILE_WRITE);
+}
+
+void print_time(unsigned long time_millis)
+{
+    Serial.print("Time: ");
+    Serial.print(time_millis/1000);
+    Serial.print("s - ");
 }
