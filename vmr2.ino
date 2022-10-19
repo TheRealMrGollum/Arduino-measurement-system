fc3be0900879cf4b51a8638fa6e776b056f0968e
TheRealMrGollum <101318597+TheRealMrGollum@users.noreply.github.com>
2022-10-19 17:48:44 +0200

    Update vmr2.ino

--- vmr2.ino
+++ vmr2.ino
@@ -1,13 +1,16 @@
 #include <SPI.h>
 #include <SD.h>
-#define INTERVAL_1 10
+#define INTERVAL_1 10           //interval lenght [ms] - 10 means it repeats 100 times per second
 #define INTERVAL_2 1000
 #define INTERVAL_3 60000
-unsigned long time_1=0, time_2=0, time_3=0;
-File myFile;
-int analogInput = 0;
-String Measurement = "Measurement", Space="  ";
-float Vout = 0.000, Iout = 0.00, Wvyst=0.00 , WH=0.00, Vin, nap, R1 = 3, R2 = 22,Vref=1.1;
+unsigned long time_1=0, time_2=0, time_3=0;                        //do not change
+File myFile;                                                       //change for file name
+int analogInput = 0;                                               //analog pin (do not use 4 or 5)
+String Measurement = "Measurement", Space="  ";                    //saved string for less code writing :)
+float Vout = 0.000, Iout = 0.00, Wvyst=0.00 , WH=0.00, 
+float Vin, Voltage;               
+float R1 = 3, R2 = 22;          //change values of R1 and R2 (MUST BE SAME AS IN CIRCUIT!)
+float Vref=1.1;                 //reference voltage 
 bool FirstMeasurement = true;
 
 void setup()
@@ -20,7 +23,7 @@ void setup()
     while (1);
   }
   myFile = SD.open(Measurement, FILE_WRITE);
-  Serial.println("Card inserted and it is working!");
+  Serial.println("Card inserted and it is working!"); //measurement can start
   analogReference(INTERNAL);
 }
 
@@ -28,45 +31,45 @@ void print_time(unsigned long time_millis); //only showed in serial monitoring
 
 void loop()
 {     
-      if(millis() >= time_1 + INTERVAL_1)
+      if(millis() >= time_1 + INTERVAL_1) //measure every 10ms
       {
         time_1 +=INTERVAL_1;
         measurement();
       } 
-      if(millis() >= time_2 + INTERVAL_2)
+      if(millis() >= time_2 + INTERVAL_2) //calculate every second
       {
         time_2 +=INTERVAL_2;
         print_time(time_2);
-        //Serial.println("prebieha vypocet");
+        //Serial.println("calculation is in progress");  //remove comment for debugging, leave it for faster processing  
         calculate();
       }
-      if(millis() >= time_3 + INTERVAL_3)
+      if(millis() >= time_3 + INTERVAL_3)   //save data every 60 seconds
       {
         time_3 +=INTERVAL_3;
         print_time(time_3);
-        Serial.println("Saving data on SD card");
-        sdcard();
+        //Serial.println("Saving data on SD card");   //remove comment for debugging, leave it for faster processing  
+        sdcard();                                     //calling function for saving
       }
           
 }
 
 void measurement()
-{   
+{   //here you can calibrate ADC, you must measure along with multimeter for more accurate measurement
     if ((analogRead(analogInput) <40)&(analogRead(analogInput) >35))Vin = ((analogRead(analogInput)+5) * Vref) / 1024.00;
     else if ((analogRead(analogInput) <35)&(analogRead(analogInput) >20))Vin = ((analogRead(analogInput)+1) * Vref) / 1024.00;
     else if ((analogRead(analogInput) <20)&(analogRead(analogInput) >3))Vin = ((analogRead(analogInput)+2) * Vref) / 1024.00;
     else if (analogRead(analogInput) > 0)Vin = ((analogRead(analogInput)) * Vref) / 1024.00;
     else Vin = ((analogRead(analogInput)) * Vref) / 1024.00;
     Vin /= (R2/(R1+R2));
-    nap+= Vin;
+    Voltage+= Vin;
 }
 
-void calculate()
-{
-  nap = nap/100; //Median of voltage
-  Serial.print("Median of voltage ");
-  Serial.println(nap*1000); // display in mV in serial
-  Vout=nap*1000; Iout =(nap)/(R1+R2); Wvyst = (nap) * (Iout); Iout*=1000; //V -> mV, A , W, A -> mA, 
+void calculate()    //change if you know what are you doing
+{     
+  Voltage = Voltage/100; //Median of voltage
+  //Serial.print("Median of voltage ");   //remove comment for debugging, leave it for faster processing  
+  //Serial.println(Voltage*1000);         // display in mV in serial
+  Vout=Voltage*1000; Iout =(Voltage)/(R1+R2); Wvyst = (Voltage) * (Iout); Iout*=1000; //V -> mV, A , W, A -> mA, 
   
   WH += (Wvyst*1000); // W -> mW
   WH /= 3600;      // mW + time = mWH
@@ -81,14 +84,14 @@ void calculate()
 
 void sdcard()
 {
-  myFile.close(); //Save data
-  Serial.println("Súbor uložený: " + Measurement); 
-  myFile = SD.open(Measurement, FILE_WRITE); //Open file and continue where ended
+  myFile.close();                                   //Save data
+  //Serial.println("File saved: " + Measurement);   //remove comment for debugging, leave it for faster processing
+  myFile = SD.open(Measurement, FILE_WRITE);        //Open file and continue where ended
 }
 
 void print_time(unsigned long time_millis)
 {
-    Serial.print("Time: ");
-    Serial.print(time_millis/1000);
-    Serial.print("s - ");
+    //Serial.print("Time: ");                       //remove comment for debugging, leave it for faster processing
+    //Serial.print(time_millis/1000);
+    //Serial.print("s - ");
 }
